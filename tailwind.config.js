5919c51423474ab6f2aa4d5e74dc3d7491a58df9
saminyasar004 <yasarsamin57@gmail.com>
2021-12-23 11:06:00 +0600

    Account, Reset Password, Cart page Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
                 primary: "#ff523b",
                 secondary: "#415161",
                 footer: "#313131",
+                "form-border": "#d3ced2",
+                "input-border": "#e3e3e3",
+                "input-border-focus": "#66afe9",
+                "tr-odd-bg": "#e7e7e7",
+                "tr-even-bg": "transparent",
             },
             backgroundImage: {
                 offer: "-webkit-gradient(linear, left top, left bottom, from(#e2e2e29e)), url('../img/offer-banner-01.jpg')",
@@ -29,6 +34,8 @@ module.exports = {
             },
             borderWidth: {
                 "2rem": "0.125rem",
+                "1rem": "0.1rem",
+                "1-2rem": "0.120rem",
             },
             lineHeight: {
                 12: "4rem",
